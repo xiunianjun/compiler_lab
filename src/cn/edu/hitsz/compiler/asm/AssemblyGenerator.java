753c2dd03c326d8175084fc43e17372aba763a4e
xiunian <1776527992@qq.com>
2023-11-08 01:49:54 +0800

    lab4 finished

--- src/cn/edu/hitsz/compiler/asm/AssemblyGenerator.java
+++ src/cn/edu/hitsz/compiler/asm/AssemblyGenerator.java
@@ -1,9 +1,16 @@
 package cn.edu.hitsz.compiler.asm;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
+import cn.edu.hitsz.compiler.ir.IRImmediate;
+import cn.edu.hitsz.compiler.ir.IRValue;
+import cn.edu.hitsz.compiler.ir.IRVariable;
 import cn.edu.hitsz.compiler.ir.Instruction;
+import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -21,6 +28,46 @@ import java.util.List;
  * @see AssemblyGenerator#run() 代码生成与寄存器分配
  */
 public class AssemblyGenerator {
+    public AssemblyGenerator() {
+        for (int i = 0; i <= 6; i ++) {
+            registers.put(i, false);
+        }
+    }
+
+    class RegisterInfo {
+        RegisterInfo() {
+            refcnt = 1;
+        }
+        public int getRegisterNo() {
+            return registerNo;
+        }
+
+        public void setRegisterNo(int registerNo) {
+            this.registerNo = registerNo;
+        }
+
+        private int registerNo = Integer.MAX_VALUE;
+        private int refcnt = 0;
+
+        public void IncreaseRefcnt() {
+            refcnt ++;
+        }
+
+        public void DecreaseRefcnt() {
+            refcnt --;
+        }
+
+        public int getRefcnt() {
+            return refcnt;
+        }
+    };
+
+    // for register renaming
+    // register coding: 0-6 [t0-t6]; register info: refcnt, occupied variable
+    Map<Integer, Boolean> registers = new HashMap<>();
+    Map<IRVariable, RegisterInfo> variables = new HashMap<>();
+    List<Instruction> itrs = new ArrayList<>();
+    List<String> asm = new ArrayList<>();
 
     /**
      * 加载前端提供的中间代码
@@ -32,7 +79,155 @@ public class AssemblyGenerator {
      */
     public void loadIR(List<Instruction> originInstructions) {
         // TODO: 读入前端提供的中间代码并生成所需要的信息
-        throw new NotImplementedException();
+        // a fix instruction mode: XX, variable, variable, imm/variable
+        for (Instruction itr : originInstructions) {
+            switch (itr.getKind()) {
+                case MOV:
+                    itrs.add(itr);
+                    break;
+                case ADD:
+                    if (itr.getLHS().isImmediate() && itr.getRHS().isImmediate()) {
+                        itrs.add(Instruction.createMov(itr.getResult(), IRImmediate.of(Integer.parseInt(itr.getRHS().toString()) + Integer.parseInt(itr.getLHS().toString()))));
+                    } else if (itr.getLHS().isImmediate() || itr.getRHS().isImmediate()) {
+                        IRValue var = itr.getLHS().isImmediate() ? itr.getRHS() : itr.getLHS();
+                        IRValue imm = itr.getLHS().isImmediate() ? itr.getLHS() : itr.getRHS();
+                        itrs.add(Instruction.createAdd(itr.getResult(), var, imm));
+                    } else {
+                        itrs.add(itr);
+                    }
+                    break;
+                case SUB:
+                    if (itr.getLHS().isImmediate() && itr.getRHS().isImmediate()) {
+                        itrs.add(Instruction.createMov(itr.getResult(), IRImmediate.of(Integer.parseInt(itr.getLHS().toString()) - Integer.parseInt(itr.getRHS().toString()))));
+                    } else if (itr.getLHS().isImmediate()) {
+                        // change to a two-register calculation
+                        IRVariable t = IRVariable.temp();
+                        itrs.add(Instruction.createMov(t, itr.getLHS()));
+                        itrs.add(Instruction.createSub(itr.getResult(), t, itr.getRHS()));
+                    } else{
+                        itrs.add(itr);
+                    }
+                    break;
+                case MUL:
+                    if (itr.getLHS().isImmediate() && itr.getRHS().isImmediate()) {
+                        itrs.add(Instruction.createMov(itr.getResult(), IRImmediate.of(Integer.parseInt(itr.getRHS().toString()) * Integer.parseInt(itr.getLHS().toString()))));
+                    } else if (itr.getLHS().isImmediate() || itr.getRHS().isImmediate()) {
+                        // change to a three-register calculation
+                        if (itr.getLHS().isImmediate()) {
+                            IRVariable t = IRVariable.temp();
+                            itrs.add(Instruction.createMov(t, itr.getLHS()));
+                            itrs.add(Instruction.createMul(itr.getResult(), t, itr.getRHS()));
+                        } else {
+                            IRVariable t = IRVariable.temp();
+                            itrs.add(Instruction.createMov(t, itr.getRHS()));
+                            itrs.add(Instruction.createMul(itr.getResult(), itr.getLHS(), t));
+                        }
+                    } else {
+                        itrs.add(itr);
+                    }
+                    break;
+                case RET:
+                    itrs.add(itr);
+                    break;
+            }
+        }
+
+        // record the variable refcnt info
+        for (Instruction itr : itrs) {
+            switch (itr.getKind()) {
+                case MOV:
+                    if (variables.containsKey(itr.getResult())) {
+                        variables.get(itr.getResult()).IncreaseRefcnt();
+                    } else {
+                        variables.put(itr.getResult(), new RegisterInfo());
+                    }
+                    if (itr.getFrom().isIRVariable()) {
+                        if (variables.containsKey(itr.getFrom())) {
+                            variables.get(itr.getFrom()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) (itr.getFrom()), new RegisterInfo());
+                        }
+                    }
+                    break;
+                case RET:
+                    if (itr.getReturnValue().isIRVariable()) {
+                        if (variables.containsKey(itr.getReturnValue())) {
+                            variables.get(itr.getReturnValue()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) itr.getReturnValue(), new RegisterInfo());
+                        }
+                    }
+                    break;
+                case ADD:
+                    if (variables.containsKey(itr.getResult())) {
+                        variables.get(itr.getResult()).IncreaseRefcnt();
+                    } else {
+                        variables.put(itr.getResult(), new RegisterInfo());
+                    }
+                    if (itr.getLHS().isIRVariable()) {
+                        if (variables.containsKey(itr.getLHS())) {
+                            variables.get(itr.getLHS()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) itr.getLHS(), new RegisterInfo());
+                        }
+                    }
+                    if (itr.getRHS().isIRVariable()) {
+                        if (variables.containsKey(itr.getRHS())) {
+                            variables.get(itr.getRHS()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) itr.getRHS(), new RegisterInfo());
+                        }
+                    }
+                    break;
+                case SUB:
+                    if (variables.containsKey(itr.getResult())) {
+                        variables.get(itr.getResult()).IncreaseRefcnt();
+                    } else {
+                        variables.put(itr.getResult(), new RegisterInfo());
+                    }
+                    if (itr.getLHS().isIRVariable()) {
+                        if (variables.containsKey(itr.getLHS())) {
+                            variables.get(itr.getLHS()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) itr.getLHS(), new RegisterInfo());
+                        }
+                    }
+                    if (itr.getRHS().isIRVariable()) {
+                        if (variables.containsKey(itr.getRHS())) {
+                            variables.get(itr.getRHS()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) itr.getRHS(), new RegisterInfo());
+                        }
+                    }
+                    break;
+                case MUL:
+                    if (variables.containsKey(itr.getResult())) {
+                        variables.get(itr.getResult()).IncreaseRefcnt();
+                    } else {
+                        variables.put(itr.getResult(), new RegisterInfo());
+                    }
+                    if (itr.getLHS().isIRVariable()) {
+                        if (variables.containsKey(itr.getLHS())) {
+                            variables.get(itr.getLHS()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) itr.getLHS(), new RegisterInfo());
+                        }
+                    }
+                    if (itr.getRHS().isIRVariable()) {
+                        if (variables.containsKey(itr.getRHS())) {
+                            variables.get(itr.getRHS()).IncreaseRefcnt();
+                        } else {
+                            variables.put((IRVariable) itr.getRHS(), new RegisterInfo());
+                        }
+                    }
+                    break;
+            }
+        }
+
+        // for debug
+        for (Instruction itr : itrs) {
+            System.out.println(itr);
+        }
     }
 
 
@@ -47,7 +242,87 @@ public class AssemblyGenerator {
      */
     public void run() {
         // TODO: 执行寄存器分配与代码生成
-        throw new NotImplementedException();
+        // when a variable's refcnt drop to zero, it should release the register immediately
+        for (Instruction itr : itrs) {
+            StringBuilder sb = new StringBuilder();
+            switch (itr.getKind()) {
+                case MOV:
+                    if (itr.getFrom().isImmediate()) {
+                        // li reg, imm
+                        int reg = getRegister(itr.getResult());
+                        sb.append("li t" + new Integer(reg).toString() + ", " + Integer.parseInt(itr.getFrom().toString()));
+                    } else {
+                        // mv reg1, reg2
+                        int reg1 = getRegister(itr.getResult());
+                        int reg2 = getRegister(itr.getFrom());
+                        if (reg1 != reg2)
+                            sb.append("mv t" + new Integer(reg1).toString() + ", t" + new Integer(reg2).toString());
+                    }
+                    break;
+                case RET:
+                    break;
+                case ADD:
+                    int reg1 = getRegister(itr.getResult());
+                    int reg2 = getRegister(itr.getLHS());
+                    if (itr.getRHS().isIRVariable()) {
+                        int reg3 = getRegister(itr.getRHS());
+                        sb.append("add t" + new Integer(reg1).toString() + ", t" + new Integer(reg2).toString() + ", t" + new Integer(reg3).toString());
+                    } else {
+                        sb.append("addi t" + new Integer(reg1).toString() + ", t" + new Integer(reg2).toString() + ", " + itr.getRHS().toString());
+                    }
+                    break;
+                case SUB: // mode: subi reg1, reg2, imm  |  sub reg1, reg2, reg3
+                    reg1 = getRegister(itr.getResult());
+                    reg2 = getRegister(itr.getLHS());
+                    if (itr.getRHS().isIRVariable()) {
+                        int reg3 = getRegister(itr.getRHS());
+                        sb.append("sub t" + new Integer(reg1).toString() + ", t" + new Integer(reg2).toString() + ", t" + new Integer(reg3).toString());
+                    } else {
+                        sb.append("subi t" + new Integer(reg1).toString() + ", t" + new Integer(reg2).toString() + ", " + itr.getRHS().toString());
+                    }
+                    break;
+                case MUL:
+                    reg1 = getRegister(itr.getResult());
+                    reg2 = getRegister(itr.getLHS());
+                    int reg3 = getRegister(itr.getRHS());
+                    sb.append("mul t" + new Integer(reg1).toString() + ", t" + new Integer(reg2).toString() + ", t" + new Integer(reg3).toString());
+                    break;
+            }
+            asm.add(sb.toString());
+            proccessInvalid();
+        }
+    }
+
+    private void proccessInvalid() {
+        for (Map.Entry<IRVariable, RegisterInfo> en : variables.entrySet()) {
+            if (en.getValue().getRefcnt() == 0 && en.getValue().getRegisterNo() != Integer.MAX_VALUE) {
+                registers.put(en.getValue().registerNo, false); // release
+                en.getValue().setRegisterNo(Integer.MAX_VALUE);
+            }
+        }
+    }
+
+    private int getRegister(IRValue result) {
+        System.out.println(result.toString());
+        int registerNo = variables.get(result).getRegisterNo();
+        if (registerNo != Integer.MAX_VALUE) {
+            variables.get(result).DecreaseRefcnt();
+            return registerNo;
+        }
+
+        // a steal
+        for (Map.Entry<Integer, Boolean> en : registers.entrySet()) {
+            if (en.getValue())  continue; // is occupied
+            registerNo = en.getKey();
+            variables.get(result).setRegisterNo(en.getKey());
+            break;
+        }
+
+        if (registerNo == Integer.MAX_VALUE)
+            throw new RuntimeException();
+        registers.put(registerNo, true);
+        variables.get(result).DecreaseRefcnt();
+        return registerNo;
     }
 
 
@@ -58,7 +333,7 @@ public class AssemblyGenerator {
      */
     public void dump(String path) {
         // TODO: 输出汇编代码到文件
-        throw new NotImplementedException();
+        FileUtils.writeLines(path, asm.stream().toList());
     }
 }
 
