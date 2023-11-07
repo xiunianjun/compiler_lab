03f1d0e3cb37eefd52e3cee07fa30aacf8a3ac23
xiunian <1776527992@qq.com>
2023-11-07 22:43:33 +0800

    lab3 finished

--- src/cn/edu/hitsz/compiler/parser/IRGenerator.java
+++ src/cn/edu/hitsz/compiler/parser/IRGenerator.java
@@ -1,14 +1,20 @@
 package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
+import cn.edu.hitsz.compiler.ir.IRImmediate;
+import cn.edu.hitsz.compiler.ir.IRValue;
+import cn.edu.hitsz.compiler.ir.IRVariable;
 import cn.edu.hitsz.compiler.ir.Instruction;
 import cn.edu.hitsz.compiler.lexer.Token;
+import cn.edu.hitsz.compiler.lexer.TokenKind;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // TODO: 实验三: 实现 IR 生成
 
@@ -16,15 +22,64 @@ import java.util.List;
  *
  */
 public class IRGenerator implements ActionObserver {
+    private Stack<IRValue> irStack = new Stack<>();
+    private List<Instruction> irs = new ArrayList<>();
 
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
         // TODO
+        if (currentToken.getKindId().equals("IntConst")) {
+            irStack.push(IRImmediate.of(Integer.parseInt(currentToken.getText())));
+        } else if (currentToken.getKindId().equals("id")) {
+            irStack.push(IRVariable.named(currentToken.getText()));
+        }
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
+        if (irStack.empty()) {
+            return;
+        }
         // TODO
+        switch (production.index()) {
+            case 6:
+                IRValue irv = irStack.peek();
+                irStack.pop();
+                irs.add(Instruction.createMov(((IRVariable)irStack.peek()), irv));
+                irStack.pop();
+                break;
+            case 7:
+                irs.add(Instruction.createRet(irStack.peek()));
+                irStack.pop();
+                break;
+            case 8:
+                IRValue lhs = irStack.peek();
+                irStack.pop();
+                IRValue rhs = irStack.peek();
+                irStack.pop();
+                IRVariable t = IRVariable.temp();
+                irs.add(Instruction.createAdd(t, rhs, lhs));
+                irStack.push(t);
+                break;
+            case 9:
+                lhs = irStack.peek();
+                irStack.pop();
+                rhs = irStack.peek();
+                irStack.pop();
+                t = IRVariable.temp();
+                irs.add(Instruction.createSub(t, rhs, lhs));
+                irStack.push(t);
+                break;
+            case 11:
+                lhs = irStack.peek();
+                irStack.pop();
+                rhs = irStack.peek();
+                irStack.pop();
+                t = IRVariable.temp();
+                irs.add(Instruction.createMul(t, rhs, lhs));
+                irStack.push(t);
+                break;
+        }
     }
 
 
@@ -40,7 +95,7 @@ public class IRGenerator implements ActionObserver {
 
     public List<Instruction> getIR() {
         // TODO
-        throw new NotImplementedException();
+        return irs;
     }
 
     public void dumpIR(String path) {
--- src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SemanticAnalyzer.java
@@ -2,32 +2,61 @@ package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
+import cn.edu.hitsz.compiler.lexer.TokenKind;
 import cn.edu.hitsz.compiler.parser.table.Production;
 import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.Term;
+import cn.edu.hitsz.compiler.symtab.SourceCodeType;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
+import cn.edu.hitsz.compiler.symtab.SymbolTableEntry;
+
+import java.util.Stack;
 
 // TODO: 实验三: 实现语义分析
 public class SemanticAnalyzer implements ActionObserver {
+    private SymbolTable symbolTable;
+    private Stack<String> attributeStack = new Stack<>();
 
     @Override
     public void whenAccept(Status currentStatus) {
         // TODO: 该过程在遇到 Accept 时要采取的代码动作
+        attributeStack.clear();
     }
 
     @Override
     public void whenReduce(Status currentStatus, Production production) {
         // TODO: 该过程在遇到 reduce production 时要采取的代码动作
+        if (attributeStack.empty()) {
+            return;
+        }
+
+        // just for symbols
+        switch (production.index()) {
+            case 5:
+                attributeStack.pop();
+                break;
+            case 4:
+                if (symbolTable.has(attributeStack.peek())) {
+                    symbolTable.get(attributeStack.peek()).setType(SourceCodeType.Int);
+                }
+                attributeStack.pop();
+                break;
+        }
     }
 
     @Override
     public void whenShift(Status currentStatus, Token currentToken) {
         // TODO: 该过程在遇到 shift 时要采取的代码动作
+//        if (currentToken.getKindId().equals("int") || currentToken.getKindId().equals("id")) {
+            attributeStack.push(currentToken.getText());
+//        }
     }
 
     @Override
     public void setSymbolTable(SymbolTable table) {
         // TODO: 设计你可能需要的符号表存储结构
         // 如果需要使用符号表的话, 可以将它或者它的一部分信息存起来, 比如使用一个成员变量存储
+        symbolTable = table;
     }
 }
 
